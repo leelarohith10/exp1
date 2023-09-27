dcab180e6e50d447f10e81e406f86f1c2f2e69de
leelarohith10 <209X1A0551@gprec.ac.in>
2023-09-27 13:14:51 +0530

    todolist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firstapp';
+  title='my first app'
+  list:any[]=[];
+  addTask(item:string)
+  {
+    this.list.push({id:this.list.length,name:item})
+    console.log(item);
+
+  }
+  removeTask(id:number)
+  {
+    console.warn(id);
+    this.list = this.list.filter(item => item.id !== id);
+  }
 }
